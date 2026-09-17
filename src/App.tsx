643a87e2cfde531e9823ce3f4cef972ac812d6de
Mohaned Sameer <departcs19@gmail.com>
2026-09-17 10:14:29 +0200

    Resolve App routing conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import './App.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-<<<<<<< HEAD
-import { ToastContainer } from "react-toastify";
-import ProtuctedRoute from "./Components/ProtuctedRoute/ProtuctedRoute";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./Components/Layout/Layout";
-import NotFoundPage from "./Pages/NotFoundPage/NotFoundPage";
-import CountryPage from "./Pages/CountryPage";
-import HandlingAgentsCompanyPage from "./Pages/HandlingAgentsCompanyPage";
-import AircraftType from "./Pages/AircraftType/AircraftType";
-=======
 import { ToastContainer } from 'react-toastify';
 import ProtuctedRoute from './Components/ProtuctedRoute/ProtuctedRoute';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
@@ -19,10 +8,11 @@ import Layout from './Components/Layout/Layout';
 import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
 import CountryPage from './Pages/CountryPage';
 import HandlingAgentsCompanyPage from './Pages/HandlingAgentsCompanyPage';
+import AircraftType from './Pages/AircraftType/AircraftType';
 import AircraftRegistrationPage from './Pages/AircraftRegistrationPage';
 import CompanyInfoPage from './Pages/CompanyInfoPage';
 import AircraftSizePage from './Pages/AircraftSizePage';
->>>>>>> 2abc54ee12977f298d30bd23b0160e2bbeab55fd
+
 const queryClient = new QueryClient();
 
 function App() {
@@ -31,14 +21,10 @@ function App() {
       path: '',
       element: <Layout />,
       children: [
-        // { index: true, element:   < <Login /> </ProtuctedRoute> },
-        //  { index: true, element: <ProtuctedRoute>  <Home/> </ProtuctedRoute>  },
-        // { index: true, path: 'login', element: <Login /> },
-
         { path: '*', element: <NotFoundPage /> },
+
         {
           index: true,
-
           element: (
             <ProtuctedRoute>
               <CountryPage />
@@ -56,12 +42,15 @@ function App() {
         },
 
         {
-<<<<<<< HEAD
-          path: "AircraftType",
+          path: 'AircraftType',
           element: (
             <ProtuctedRoute>
               <AircraftType />
-=======
+            </ProtuctedRoute>
+          ),
+        },
+
+        {
           path: 'aircraftRegistration',
           element: (
             <ProtuctedRoute>
@@ -78,27 +67,24 @@ function App() {
             </ProtuctedRoute>
           ),
         },
+
         {
           path: 'aircraftSize',
           element: (
             <ProtuctedRoute>
               <AircraftSizePage />
->>>>>>> 2abc54ee12977f298d30bd23b0160e2bbeab55fd
             </ProtuctedRoute>
           ),
         },
       ],
     },
   ]);
+
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        {/* <AuthContextProvider> */}
-        <ToastContainer />
-        <RouterProvider router={router} />
-        {/* </AuthContextProvider> */}
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
 
